d0ef6fcb244a4f77e8eb3b180bc3068a5d5ea7b3
Nicholas Devenish <ndevenish@gmail.com>
2015-08-15 18:34:11 +0100

    put in the start of a shell of a config parser

--- KerbalHUD/ConfigNode.swift
+++ KerbalHUD/ConfigNode.swift
@@ -8,6 +8,48 @@
 
 import Foundation
 
-class ConfigNode {
+protocol ConfigNode {
+  var name : String { get }
   
+  func GetNode(name : String) -> ConfigNode?
+  func GetNodes(name : String) -> [ConfigNode]
+  func GetValue(name : String) -> String?
+  func GetValues(name : String) -> [String]
+}
+
+struct ConfigNodeImpl : ConfigNode {
+
+  var name : String
+
+  init(data : String) {
+    name = ""
+  }
+  
+  func GetNode(name : String) -> ConfigNode? {
+    return nil
+  }
+  func GetNodes(name : String) -> [ConfigNode] {
+    return []
+  }
+  func GetValue(name : String) -> String? {
+    return nil
+  }
+  func GetValues(name : String) -> [String] {
+    return []
+  }
+}
+
+class ConfigStringTokenizer {
+  let data : String
+  let index : Int = 0
+  
+  init(data : String) {
+    self.data = data
+  }
+  
+  func next() -> String? {
+//    let finder = NSRegularExpression(pattern: " \t\n\r={{}}", options: nil)
+    
+    
+  }
 }

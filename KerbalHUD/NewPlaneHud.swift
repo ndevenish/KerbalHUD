8c85d7224766cdd0badbdb47db1455fc954d3bdd
Nicholas Devenish <ndevenish@gmail.com>
2015-09-08 08:59:55 +0100

    Add the plane hud overlay

--- KerbalHUD/NewPlaneHud.swift
+++ KerbalHUD/NewPlaneHud.swift
@@ -33,7 +33,7 @@ class NewPlaneHud : LayeredInstrument {
   init(tools : DrawingTools) {
     var config = InstrumentConfiguration()
     config.size = Size2D(w: 1, h: 1)
-    
+    config.overlay = SVGOverlay(url: NSBundle.mainBundle().URLForResource("PlaneHUD_Overlay", withExtension: "svg")!)
     // Now do the text
     config.text.appendContentsOf([
       t("PRS: %7.3f Pa",  "v.dynamicPressure", x: 1, y: 1, align: .Left),
